38e69f19530dba071dc997c4db1c0532906bee99
Mirn4 <ampueromirna8@gmail.com>
2022-10-11 17:27:53 -0500

    Add the optional changes

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-function closewindows() {
-  const navbarOptions = document.querySelector('#navbar-options');
-  return navbarOptions.classList.replace('menu-mobile', 'nav-menu');
-}
-const openmenu = document.querySelector('#button-hamburger');
-openmenu.addEventListener('click', () => {
-  const navbarOptions = document.querySelector('#navbar-options');
-  navbarOptions.classList.replace('nav-menu', 'menu-mobile');
-  const closeIcon = document.querySelector('#x');
-  closeIcon.classList.replace('hidden', 'close-icon-visible');
+const hamburger = document.querySelector('#button-hamburger');
+// eslint-disable-next-line no-unused-vars
+const navMenu = document.querySelector('#navbar-options');
+const closeIcon = document.querySelector('#x');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('menu-mobile');
+  closeIcon.classList.toggle('close-icon-visible');
 });
 
-const closemenu = document.querySelector('#x');
-closemenu.addEventListener('click', () => {
-  closewindows();
-});
-
-const closeLinkP = document.querySelector('#links-p');
-closeLinkP.addEventListener('click', () => {
-  closewindows();
-});
-
-const closeLinkA = document.querySelector('#links-a');
-closeLinkA.addEventListener('click', () => {
-  closewindows();
-});
-
-const closeLinkC = document.querySelector('#links-c');
-closeLinkC.addEventListener('click', () => {
-  closewindows();
-});
+document.querySelectorAll('.links').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('menu-mobile');
+  closeIcon.classList.remove('close-icon-visible');
+}));
